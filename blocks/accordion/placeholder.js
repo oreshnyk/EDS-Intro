b941541b6e0d662ca6001a01af223dc37666ca23
Oleksii Reshetnyk <oleksii.reshetnyk@netcentric.biz>
2025-06-23 15:22:41 +0200

    check

--- blocks/accordion/placeholder.js
+++ blocks/accordion/placeholder.js
@@ -1,5 +1,5 @@
 export async function loadTranslate(lang = 'en') {
-    const placeholderData = 'https://docs.google.com/spreadsheets/d/1chEVgHs7LRRCfYm0olzf2Tj_AzLuA1Y6AVz0hGlheJ8/edit?gid=0#gid=0';
+    const placeholderData = 'https://main--eds-intro--oreshnyk.aem.live/placeholder.json';
     const resp = await fetch(placeholderData);
     if (!resp.ok) return {open: 'open', close: 'close'};
     const data = await resp.json();
